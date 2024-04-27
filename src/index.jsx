aca9b34ac0d59e440cc69a4e3724e8bd1a679e2a
Adashima <sapondanaisriwan@gmail.com>
2024-04-27 16:55:35 +0700

    Create: src

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "react-dom";
+import "./styles/globals.css";
+import App from "./App";
+
+const root = document.getElementById("root");
+render(<App />, root);
