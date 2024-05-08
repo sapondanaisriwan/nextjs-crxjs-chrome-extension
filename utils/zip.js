60381e2676dc89c226099e75effc75102c38d851
Adashima <sapondanaisriwan@gmail.com>
2024-05-08 20:08:09 +0700

    update zip.js

--- utils/zip.js
+++ utils/zip.js
@@ -1,11 +1,11 @@
 const { readFile, mkdir } = require("fs/promises");
-const { directory } = require("./modules/config");
+const { directory, manifestBuild } = require("./modules/config");
 const fsExists = require("fs.promises.exists");
 const AdmZip = require("adm-zip");
 const zip = new AdmZip();
 
 const run = async () => {
-  const manifest = await readFile("extension/manifest-build.json", "utf-8");
+  const manifest = await readFile(manifestBuild, "utf-8");
   const { name, version } = JSON.parse(manifest);
   const isZipExists = await fsExists("zip");
 
